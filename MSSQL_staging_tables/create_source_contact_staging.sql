ee7e09d4a14cbfc9de27771808ec2313e254d280
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-03-23 16:30:15 -0700

    Create create_source_contact_staging.sql

---
+++ MSSQL_staging_tables/create_source_contact_staging.sql
@@ -0,0 +1,25 @@
+USE [Data_Engineering]
+GO
+
+/****** Object:  Table [dbo].[STG_SOURCE_Contacts]  Script Date: 3/23/2026 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+DROP TABLE IF EXISTS [dbo].[STG_SOURCE_Contacts]
+GO
+
+CREATE TABLE [dbo].[STG_SOURCE_Contacts](
+  [account_number_external_id] [nvarchar](36),
+  [first_name] [nvarchar](50) NOT NULL,
+  [last_name] [nvarchar](50) NOT NULL,
+  [email] [nvarchar](50) NOT NULL,
+  [title] [nvarchar](50) NOT NULL,
+  [department] [nvarchar](50) NOT NULL,
+  [languages] [nvarchar](50) NOT NULL,
+  PRIMARY KEY (first_name, last_name)
+
+) ON [PRIMARY]
+GO
