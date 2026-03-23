34a4a0a3d5c5fd5a5b0dc45f6ad1748a142ed0b6
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-03-23 16:30:06 -0700

    modify source tables to include end date

--- MySQL_source_tables/create_contracts_table.sql
+++ MySQL_source_tables/create_contracts_table.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS contracts;
 CREATE TABLE contracts (
   account_number_external_id varchar(36) NOT NULL,
   contract_number varchar(36) PRIMARY KEY,
-  start_date DATE NOT NULL
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL
 );
