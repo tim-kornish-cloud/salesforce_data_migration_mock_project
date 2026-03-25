07473fc5522fb522b5c72b82ba30c4bbb7ce3b06
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-03-24 22:51:58 -0700

    extend field length in mysql table

--- MySQL_source_tables/create_contacts_table.sql
+++ MySQL_source_tables/create_contacts_table.sql
@@ -2,11 +2,11 @@ USE data_engineering;
 DROP TABLE IF EXISTS contacts;
 CREATE TABLE contacts (
   account_number_external_id varchar(36),
-  first_name varchar(50) NOT NULL,
-  last_name varchar(50) NOT NULL,
-  email varchar(50) NOT NULL,
-  title varchar(50) NOT NULL,
-  department varchar(50) NOT NULL,
-  languages varchar(50) NOT NULL,
+  first_name varchar(200) NOT NULL,
+  last_name varchar(200) NOT NULL,
+  email varchar(200) NOT NULL,
+  title varchar(200) NOT NULL,
+  department varchar(200) NOT NULL,
+  languages varchar(200) NOT NULL,
   PRIMARY KEY (first_name, last_name)
 );
