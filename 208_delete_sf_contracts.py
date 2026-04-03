7496688f05c862cafc7e047a65c546ffeff225fa
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-04-03 00:47:34 -0700

    set up contract delete script

--- 208_delete_sf_contracts.py
+++ 208_delete_sf_contracts.py
@@ -44,16 +44,16 @@ sf_token = Cred.get_token(sf_database, sf_environment)
 # create a instance of simple_salesforce to query and perform operations against salesforce with
 sf = SF_Utils.login_to_salesForce(sf_username, sf_password, sf_token)
 # query string to select records from salesforce
-account_query = "SELECT Id FROM Contact WHERE Account.Migrated_Record__c = True"
-# query salesforce and return the accounts to be deleted
-account_query_results = SF_Utils.query_salesforce(sf, account_query)
+contract_query = "SELECT Id FROM Contract WHERE SBQQ__Order__r.SBQQ__Quote__r.Migrated_Record__c = True"
+# query salesforce and return the contracts to be deleted
+contract_query_results = SF_Utils.query_salesforce(sf, contract_query)
 
-#print(account_query_results)
+#print(contract_query_results)
 # convert query results to a dataframe
-sf_accounts_df = SF_Utils.load_query_with_lookups_into_dataframe(account_query_results)
+sf_contracts_df = SF_Utils.load_query_with_lookups_into_dataframe(contract_query_results)
 # encode the dataframe before uploading to delete
-sf_accounts_df = Utils.encode_df(sf_accounts_df)
+sf_contracts_df = Utils.encode_df(sf_contracts_df)
 
-# delete migrated salesforce account records
+# delete migrated salesforce contract records
 # upload the records to salesforce for deletion
-SF_Utils.upload_dataframe_to_salesforce(sf, sf_accounts_df, 'Account', 'delete', success_file, fallout_file)
+SF_Utils.upload_dataframe_to_salesforce(sf, sf_contracts_df, 'Contract', 'delete', success_file, fallout_file)
