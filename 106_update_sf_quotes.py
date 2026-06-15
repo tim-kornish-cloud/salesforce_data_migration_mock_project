5a629e542fbaa90a98c31aa3e4fb5875c6886c0f
tim-kornish-cloud <kornish.timothy@gmail.com>
2026-06-15 14:45:22 -0600

    update comments and description

--- 106_update_sf_quotes.py
+++ 106_update_sf_quotes.py
@@ -4,6 +4,8 @@ CreatedDate: June - 14 - 2026
 Description: In salesforce configure CPQ to re-enable CPQ triggers
              Log into salesforce and query existing quotes.
              Update field SBQQ__Primary__c to true.
+             wait 1 minute.
+             Update field SBQQ__Ordered__c to true.
 """
 
 import numpy as np
@@ -29,8 +31,9 @@ pd.set_option('display.max_columns', None)
 # can have multiple environments in the same script at the same time
 environment = 'localhost'
 database = 'mssql'
+# set object name to update records into salesforce
 object = 'SBQQ__Quote__c'
-#set up directory pathway to load csv data and output fallout and success results to
+# set up directory pathway to load csv data and output fallout and success results to
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 # success file path
@@ -42,7 +45,6 @@ fallout_file = dir_path + "\\Output\\UPDATE\\FALLOUT_Update_" + environment + "_
 success_file_2 = dir_path + "\\Output\\UPDATE\\SUCCESS_Update_2_" + environment + "_" + object + "_" + database + ".csv"
 # fallout file path
 fallout_file_2 = dir_path + "\\Output\\UPDATE\\FALLOUT_Update_2_" + environment + "_" + object + "_" + database + ".csv"
-
 
 # get credentials for salesforce login
 # declare which environment this script will perform operations against,
@@ -62,7 +64,6 @@ sf_token = Cred.get_token(sf_database, sf_environment)
 sf = SF_Utils.login_to_salesForce(sf_username, sf_password, sf_token)
 
 # Query SF quotes - relate contract line to quote
-
 # query existing quotes from salesforce
 # query string to select records from salesforce
 quote_query = "SELECT Id FROM SBQQ__Quote__c WHERE Migrated_Record__c = True AND SBQQ__Primary__c = False"
@@ -74,14 +75,14 @@ sf_quotes_df = SF_Utils.load_query_with_lookups_into_dataframe(quote_query_resul
 # encode the dataframe before uploading to delete
 sf_quotes_df = Utils.encode_df(sf_quotes_df)
 
-
-
+# update quote to primary to sync quotelines with opportunity
 sf_quotes_df['SBQQ__Primary__c'] = True
 
 # update records in salesforce Quote object
 # upload the records to salesforce
 SF_Utils.upload_dataframe_to_salesforce(sf, sf_quotes_df, object, 'update', success_file, fallout_file, batch_size = 10)
 
+# set delay after updating quotes before ordering them
 print("Begin 1 minute time delay before ordering quotes.")
 time.sleep(60)
 print("Time delay over, start ordering quotes.")
@@ -97,6 +98,7 @@ sf_quotes_df = SF_Utils.load_query_with_lookups_into_dataframe(quote_query_resul
 # encode the dataframe before uploading to delete
 sf_quotes_df = Utils.encode_df(sf_quotes_df)
 
+# update all quotes to SBQQ__Ordered__c = true
 sf_quotes_df['SBQQ__Ordered__c'] = True
 
 # upload the records to salesforce
